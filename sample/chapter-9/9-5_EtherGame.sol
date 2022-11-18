cda9f56f24ff56e9be97f53b77d4278ea27f32c1
yaiwase <yaiwase@gmail.com>
2022-11-18 12:37:49 +0900

    fixed 9-5_EtherGame.sol

--- sample/chapter-9/9-5_EtherGame.sol
+++ sample/chapter-9/9-5_EtherGame.sol
@@ -8,13 +8,15 @@ contract EtherGame {
     uint public finalReward = 5 ether;
     uint public depositedWei;
 
-    mapping (address => uint) redeemableEther;
+    mapping(address => uint) redeemableEther;
 
+    // 利用者は0.5 ether を支払う。特定のマイルストーンで、自分のアカウントに入金する
     function play() external payable {
-        require(msg.value == 0.5 ether);
+        require(msg.value == 0.5 ether); // 毎プレイ0.5 ether
         uint currentBalance = depositedWei + msg.value;
         // ゲーム終了後にプレイヤーがいないことを確認する
         require(currentBalance <= finalMileStone);
+        // マイルストーンになったら、プレイヤーのアカウントに入金する
         if (currentBalance == payoutMileStone1) {
             redeemableEther[msg.sender] += mileStone1Reward;
         }
@@ -31,7 +33,7 @@ contract EtherGame {
     function claimReward() public {
         // ゲームが完了したことを確認する
         require(depositedWei == finalMileStone);
-        // 与える報酬があることを確認する
+        // 報酬があることを確認する
         require(redeemableEther[msg.sender] > 0);
         uint transferValue = redeemableEther[msg.sender];
         redeemableEther[msg.sender] = 0;
